925012b7f1243ea915af6718a9a756250d306df6
kunal-singhh <kunal.singh@freshworks.com>
2023-08-21 12:52:43 +0530

    results generated

---
+++ generateIndex.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const CURRENT_DIRECTORY = __dirname;
+
+function generateIndexFile() {
+  const htmlFiles = fs.readdirSync(CURRENT_DIRECTORY)
+    .filter(file => file.endsWith('.html'));
+
+  const links = htmlFiles.map(htmlFile => {
+    const linkText = path.basename(htmlFile, '.html');
+    return `<a href="./${htmlFile}">${linkText}</a>`;
+  });
+
+  const indexContent = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HTML File Index</title>
+</head>
+<body>
+  <h1>HTML File Index</h1>
+  <ul>
+    ${links.map(link => `<li>${link}</li>`).join('\n')}
+  </ul>
+</body>
+</html>
+`;
+
+  fs.writeFileSync(path.join(CURRENT_DIRECTORY, 'index.html'), indexContent);
+
+  console.log('Index file created.');
+}
+
+generateIndexFile();
